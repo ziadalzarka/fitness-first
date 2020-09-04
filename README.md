94cb473c1783a70da801dd62781daca0771fb803
Ziad Alzarka <ziadalzarka@gmail.com>
2020-09-04 11:39:55 +0200

    readme

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
-FITNESS FIRST GYM
-=================
+# Fitness First GYM
+
+This is a very simple Electron and AngularJS app for a local gym.
+
+![Screenshot](screenshot.png)
--- src/services/auth.service.js
+++ src/services/auth.service.js
@@ -1,55 +1,55 @@
-const serialNumber = require('serial-number');
-const bcrypt = require('bcryptjs');
+const serialNumber = require("serial-number");
+const bcrypt = require("bcryptjs");
 
-angular.module('app', [])
-  .service('auth', function($http) {
+angular.module("app", []).service("auth", function ($http) {
+  serialNumber.preferUUID = true;
 
-    serialNumber.preferUUID = true;
-
-    function getUUID() {
-      return new Promise((resolve, reject) => {
-        serialNumber((err, serial) => {
-          if (err) {
-            reject(err);
-            return;
-          }
-          resolve(serial);
-        })
-      });
-    }
-
-    async function getPayload() {
-      const uuid = await getUUID();
-      return { name: 'ahmedelhasawy', uuid };
-    }
-
-    function requestHash({ name, uuid }) {
-      const uri = `http://grayinstasave.000webhostapp.com/fitness-first/active.php?name=${name}&uuid=${uuid}`;
-      return $http.get(uri).then(res => res.data);
-    }
-
-    function validateHash({ name, uuid }, hash) {
-      if (!hash) { return false; }
-      return bcrypt.compareSync(`${name}:${uuid}`, hash)
-    }
-
-    return {
-      /**
-       * @returns {'ok'|'invalid'|'internet'} 
-       */
-      check: async function() {
-        try {
-          const payload = await getPayload();
-          try {
-            const hash = await requestHash(payload);
-            const validated = validateHash(payload, hash);
-            return validated ? 'ok' : 'invalid';
-          } catch {
-            return 'internet';
-          }
-        } catch {
-          return 'error';
+  function getUUID() {
+    return new Promise((resolve, reject) => {
+      serialNumber((err, serial) => {
+        if (err) {
+          reject(err);
+          return;
         }
+        resolve(serial);
+      });
+    });
+  }
+
+  async function getPayload() {
+    const uuid = await getUUID();
+    return { name: "ahmedelhasawy", uuid };
+  }
+
+  function requestHash({ name, uuid }) {
+    const uri = `http://grayinstasave.000webhostapp.com/fitness-first/active.php?name=${name}&uuid=${uuid}`;
+    return $http.get(uri).then((res) => res.data);
+  }
+
+  function validateHash({ name, uuid }, hash) {
+    if (!hash) {
+      return false;
+    }
+    return bcrypt.compareSync(`${name}:${uuid}`, hash);
+  }
+
+  return {
+    /**
+     * @returns {'ok'|'invalid'|'internet'}
+     */
+    check: async function () {
+      try {
+        const payload = await getPayload();
+        try {
+          const hash = await requestHash(payload);
+          const validated = validateHash(payload, hash);
+          return validated ? "ok" : "invalid";
+        } catch {
+          return "internet";
+        }
+      } catch {
+        return "error";
       }
-    };
-  });
+    },
+  };
+});
